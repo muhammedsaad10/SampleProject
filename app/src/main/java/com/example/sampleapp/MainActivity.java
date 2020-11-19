799d6024d81cc91fc5bc041705cca7bea3adec43
Saad Puthalth <saadputhalth@gmail.com>
2020-11-19 16:10:08 +0530

    first commit

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "Welcome to new Program", Toast.LENGTH_SHORT).show();
+    }
+}
